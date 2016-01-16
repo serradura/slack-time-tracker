be896b90ffb7eca7ae0b525b0c0c4d403218ad40
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2016-01-16 15:36:58 -0200

    Create TimeEntry#now

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,4 +2,10 @@ class TimeEntry < ActiveRecord::Base
   belongs_to :user
 
   validates :note, :date, :start, presence: true
+
+  def now
+    time = (Time.current - start).to_formatted_s(:time)
+
+    "#{time} - #{note}"
+  end
 end
