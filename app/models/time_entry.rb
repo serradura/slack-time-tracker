86b32c23941c3bdd7cf8ac4bfdf24278c265d761
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2016-01-16 19:11:27 -0200

    Calculate TimeEntry duration

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -3,6 +3,8 @@ class TimeEntry < ActiveRecord::Base
 
   validates :note, :date, :start, presence: true
 
+  before_update :calculate_duration
+
   def now
     diff = TimeDiffer.from_now(start)
 
@@ -16,5 +18,9 @@ class TimeEntry < ActiveRecord::Base
     time_diff  = TimeDiffer.between(start, self.end || Time.current)
 
     "#{date}\t\t|\t#{start_time}\t\t|\t#{end_time}\t\t|\t#{time_diff}\t\t|\t#{note}"
+  end
+
+  def calculate_duration
+    self.duration = self.end - start if start.present? && self.end.present?
   end
 end
