19c4944d107fd34f5c79f83b87b78562f642498a
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2016-01-20 00:26:05 -0200

    Create SlashCommand::CommandsContainer

---
+++ app/lib/slash_command/commands_container.rb
@@ -0,0 +1,35 @@
+module SlashCommand
+  class CommandsContainer
+    attr_reader :unknown
+
+    delegate :map, :each, to: :@relation
+
+    def initialize(config)
+      @cache = {}
+      setup(config)
+      build
+    end
+
+    def fetch(name)
+      @cache.fetch(name, @unknown)
+    end
+
+    private
+
+    def setup(config)
+      @help = config.fetch(:help)
+      @unknown = config.fetch(:unknown)
+
+      @relation = Array(config.fetch(:available))
+      @relation.concat Array(config.fetch(:example, nil)) unless Rails.env.production?
+      @relation.push config.fetch(:help)
+    end
+
+    def build
+      @relation.each {|command| @cache[command.name!] = command }
+
+      @options.freeze
+      @relation.freeze
+    end
+  end
+end
