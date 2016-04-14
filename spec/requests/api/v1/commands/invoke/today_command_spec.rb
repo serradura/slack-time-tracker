8d5020fe40e8d605ba3d0bef8e70120d9f12ebea
Guilherme Hashioka <guilherme.hashioka@gmail.com>
2016-04-14 10:35:54 -0300

    Fix guard offenses

--- spec/requests/api/v1/commands/invoke/today_command_spec.rb
+++ spec/requests/api/v1/commands/invoke/today_command_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.describe "POST /api/v1/commands/invoke", type: :request do
   let(:payload) { create(:slack_payload).tap {|pay| pay.text = payload_text } }
   let(:payload_text) { "today" }
-  #let(:current_user) { User.last }
   let(:template_command) { SlashCommand::Commands::Template }
   let(:current_command) { SlashCommand::Commands::Today }
 
@@ -21,7 +20,7 @@ RSpec.describe "POST /api/v1/commands/invoke", type: :request do
       let(:payload_text) { "in foo bar" }
       let(:number_of_headers) { 1 }
       let(:number_of_inputs) { 1 }
-      
+
       before do
         payload.text = "today"
         post api_v1_commands_invoke_path, payload.to_h
@@ -29,8 +28,8 @@ RSpec.describe "POST /api/v1/commands/invoke", type: :request do
 
       it "return the number of registered activities" do
         expect(response).to have_http_status(200)
-        lineNumber = response.body.split("\n").length
-        expect(lineNumber).to be == number_of_inputs + number_of_headers
+        line_number = response.body.split("\n").length
+        expect(line_number).to be == number_of_inputs + number_of_headers
       end
 
       it "checks if foo bar text appears on response" do
@@ -41,10 +40,10 @@ RSpec.describe "POST /api/v1/commands/invoke", type: :request do
     end
 
     context "there is no registered activity" do
-	    it "responds with the default 'no activity' message" do
-	      expect(response).to have_http_status(200)
-	      expect(response.body).to be == current_command::NO_HISTORY_ACTIVITY
-	    end
-		end
+      it "responds with the default 'no activity' message" do
+        expect(response).to have_http_status(200)
+        expect(response.body).to be == current_command::NO_HISTORY_ACTIVITY
+      end
+    end
   end
 end
